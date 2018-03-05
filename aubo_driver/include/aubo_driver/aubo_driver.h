b046325510fd86de6357412d455e19d25c2b09b1
Rein Appeldoorn <rein@ruvu.nl>
2018-03-05 18:03:11 +0100

    wip: Working driver

--- aubo_driver/include/aubo_driver/aubo_driver.h
+++ aubo_driver/include/aubo_driver/aubo_driver.h
@@ -44,176 +44,45 @@ double get_robot_one_io_status( our_contorl_io_type  io_type, our_contorl_io_mod
 #include "aubo_driver/serviceinterface.h"
 #include "sensor_msgs/JointState.h"
 
-#define BufferQueueSize 2000
-#define THRESHHOLD 0.000001
+#include <actionlib/server/simple_action_server.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
+
 #define ARM_DOF 6
-#define MAXALLOWEDDELAY 20
 
 namespace aubo_driver
 {
-    struct PlanningState
-    {
-    //    double currentPosition[aubo_robot_namespace::ARM_DOF];
-    //    double currentVelocity[ARM_DOF];
-    //    double currentAcceleration[ARM_DOF];
-        double currentJointPos[ARM_DOF];
-    };
-    enum ROBOT_CONTROLLER_MODE
-    {
-        ROBOT_CONTROLLER=0, //
-        ROS_CONTROLLER
-    };
+  typedef actionlib::SimpleActionServer<control_msgs::FollowJointTrajectoryAction> ActionServerType;
 
-    class BufQueue
-    {
-        public:
-            BufQueue()                             //构造函数，置空队列
-            {
-                front = rear = 0;
-            }
-            ~BufQueue(){}    //析构函数
-            bool Empty()                          //判断队列是否为空
-            {
-                if(front == rear)
-                    return true;
-                else
-                    return false;
-            }
-
-            void EnQueue(PlanningState x)
-            {
-                if(((rear + 1) % BufferQueueSize) == front)             //判断队列是否已满
-                    DeQueue();
-
-                rear = (rear + 1) % BufferQueueSize;             //移动尾指针指向下一个空间
-                buf[rear] = x;                        //元素x入队
-            }
-
-            PlanningState DeQueue()                    //队头元素出栈
-            {
-                front = (front + 1) % BufferQueueSize;        //移动队头指针指向下一个空间，即被删元素所在位置
-                return buf[front];               //返回被删除的元素的值
-            }
-
-            int GetQueueSize()
-            {
-                return ((rear+BufferQueueSize-front)%BufferQueueSize);
-            }
-    private:
-            PlanningState buf[BufferQueueSize];                  //存放队列的数组
-            int front,rear;                      //头指针与尾指针
-    };
-
-    class RobotState
-    {
-    public:
-//        RobotState();
-//        ~RobotState();
-        aubo_robot_namespace::JointStatus jointStatus[6];
-        aubo_robot_namespace::RobotDiagnosis robotDiagnosisInfo;
-        bool IsRealRobotExist;
-        bool isRobotControllerConnected;
-        ROBOT_CONTROLLER_MODE robot_controller;
-        aubo_robot_namespace::RobotState state;
-        aubo_robot_namespace::RobotErrorCode code;
-        unsigned char getRobotMode();
-        bool isReady();
-
-        void setDisconnected();
-
-        bool getNewDataAvailable();
-        void finishedReading();
-    };
 
     class AuboDriver
     {
         public:
-            AuboDriver();
+            AuboDriver(std::string ip, int port, double io_flag_delay);
             ~AuboDriver();
-            bool road_point_compare(double *point1, double *point2);
-
-            double* getCurrentPosition();
-            void setCurrentPosition(double *target);
-            double* getTagrtPosition();
-            void setTagrtPosition(double *target);
-
-            void updateControlStatus();
-            void run();
-            bool setIO(aubo_msgs::SetIORequest& req, aubo_msgs::SetIOResponse& resp);
-
-        public:
-            static bool IsRealRobotExist;
-            static bool startMove;
-            static int controlOption;
-            static double lastRecievePoint[ARM_DOF];
-            static std::string jointname[ARM_DOF];
-
-            int BufferSize;
-            ServiceInterface robotService;      //send
-            ServiceInterface robotService1;     //receive
-
-            RobotState rs;
-//            std::thread* mb_publish_thread_;
-
-
-            BufQueue  bufQueue;
-            aubo_msgs::JointPos cur_pos;
-            ros::Publisher jointstates_pub;
-            ros::Publisher jointtarget_pub;
-            ros::Publisher robot_status_pub;
-            ros::Subscriber teach_subs;
-            ros::Subscriber moveAPI_subs;
-            ros::Subscriber moveIt_controller_subs;
-            ros::Publisher io_pub;
 
         private:
-            void MoveItPosCallback(const sensor_msgs::JointState::ConstPtr &msg);
-            void AuboAPICallback(const std_msgs::Float32MultiArray::ConstPtr &msg);
-            void TeachCallback(const std_msgs::Float32MultiArray::ConstPtr &msg);
-            void chatterCallback1(const std_msgs::Float32MultiArray::ConstPtr &msg);
-            void chatterCallback3(const aubo_msgs::IOState::ConstPtr &msg);
+
+            bool setIO(aubo_msgs::SetIORequest& req, aubo_msgs::SetIOResponse& resp);
+
+            ServiceInterface robot_service_;
+
+            ros::Publisher joint_state_publisher_;
+            ros::Publisher robot_status_publisher_;
+            ros::Publisher io_publisher_;
+
+            std::shared_ptr<ActionServerType> action_server_;
+            void executeCallback(const control_msgs::FollowJointTrajectoryGoalConstPtr& goal);
+
             void timerCallback(const ros::TimerEvent& e);
-            bool setRobotJointsByMoveIt();
-            void plantypeCallback(const std_msgs::Int32MultiArray::ConstPtr &msg);
             void publishIOMsg();
 
-            bool reverse_connected_;
-
-            ros::NodeHandle nh;
-            ros::Publisher  rib_pub;
-            ros::Subscriber plan_type_sub;
-            ros::Timer timer;            
-            ros::Timer io_publish_timer;
+            ros::Timer timer;
 
             ros::ServiceServer io_srv_;
-            std::thread* mb_publish_thread_;
 
             double io_flag_delay_;
-            static int ribbuffersize;
-            static int controlMode;
-            std_msgs::Int32MultiArray ribstatus;
-            industrial_msgs::RobotStatus robotstatus;
-            int oldribstatus[3];
-            static int ControllerConnectedFlag;
-            static bool dataRecieved;
-            static int dataCount;
-
-            static double currentJoints[ARM_DOF];
-            static double targetPoint[ARM_DOF];
     };
 
-    enum ControMode
-    {
-        Teach = 0,
-        SendTargetGoal,
-        SynchronizeWithRealRobot
-    };
-
-    enum ControlOption
-    {
-        AuboAPI = 0,
-        RosMoveIt
-    };
 }
 
 #endif /* AUBO_DRIVER_H_ */
